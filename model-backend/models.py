dd3c51ae14314164aeb1ed2a2c48386d71a1451b
Devasy23 <20bce057@nirmauni.ac.in>
2023-09-10 23:02:33 +0530

    commit added autoenc model

--- model-backend/models.py
+++ model-backend/models.py
@@ -18,7 +18,7 @@ masks = {
   'laplacian5': np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]),
   'laplacian6': np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]),
   'soebel1': np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]),
-  # 'soebel2': np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]),
+  'soebel2': np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]),
 }
 
 # def applyLaplacian(laplacianMask, img):
